35ffd1e8a53a8a055f77d400809a7b94b03a4fb0
MarkusSecundus <kubahronik.g@gmail.com>
2024-02-07 23:18:18 +0100

    add warning that shows on WebGL build to use Firefox (because Chromium has issues with TMPro disappearing)

---
+++ Assets/Scripts/OneShots/PlatformDependentAction.cs
@@ -0,0 +1,23 @@
+using MarkusSecundus.PhysicsSwordfight.Utils.Serialization;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlatformDependentAction : MonoBehaviour
+{
+    [SerializeField] bool _runOnStartup = false;
+    [SerializeField] SerializableDictionary<RuntimePlatform[], UnityEvent> _actionByPlatforms;
+
+    void Start()
+    {
+        if (!_runOnStartup) return;
+        foreach(var (platforms, action) in _actionByPlatforms.Values)
+        {
+            if (Array.IndexOf(platforms, Application.platform) >= 0)
+                action?.Invoke();
+        }
+    }
+
+}
